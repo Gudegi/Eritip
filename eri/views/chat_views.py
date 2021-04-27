78ca7fe351e20eec7c34959bd2761158c02b3f0a
Gudegi <kang361973@gmail.com>
2021-04-27 18:58:17 +0900

    fix chat

--- eri/views/chat_views.py
+++ eri/views/chat_views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from eri.models import User
-import requests, json, time
+import  json, time
 
 bp = Blueprint('chat', __name__, url_prefix='/chat')
 
@@ -10,11 +10,12 @@ def response():
     query = dict(request.form)['query']
     
     #User.query.filter(User.name.like('%플라스크%')).all()
+    res = '안녕하세요? '+query + "" + time.ctime()
     info = User.query.all() #모든 정보의 리스트에서, for문을 돌려서  이름값 i가 in query라면 학번 리턴.
     for i in info:
         if i.name in query:
             name =  i.name
             gaeIn = i.gaeIn 
-    
-    res = name + '님의 학번은' + gaeIn + '입니다.'
+            res = name + '님의 학번은' + gaeIn + '입니다.'
+     
     return jsonify({"response" : res})
