c127af9fccbb10ec23f2333e6519d1b255912ca6
Gudegi <kang361973@gmail.com>
2021-05-28 05:58:30 +0900

    fix dockerfile10

--- eri/views/chat_views.py
+++ eri/views/chat_views.py
@@ -5,7 +5,6 @@ from eri import db
 from eri.models import ChatLog
 import re, json, os, ast, datetime
 import pandas as pd
-import numpy as np
 import jpype
 from konlpy.tag import Kkma
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -13,10 +12,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 
 cwd = os.getcwd()
-model = load_model(cwd+'/eri/eg/brain/Model/ERITIP_1DCNN.h5')
+model = load_model(cwd+'/eri/eg/brain/Model/ERITIP_1DCNN_98per.h5')
 noun_kkm = ['NNP', 'NNG', 'OL', 'NP','VV', 'VA' ] #꼬꼬마에서 명사들 품사
 kkma = Kkma()
 tokenizer = Tokenizer()
+with open(cwd+'/eri/eg/brain/Model/wordIndex.json') as json_file:
+  word_index = json.load(json_file)
+  tokenizer.word_index = word_index
+
 #품사 태깅 제거 명사 토크나이징
 def tokenizer_kkma_noun2(doc):
   jpype.attachThreadToJVM()
@@ -51,10 +54,10 @@ def response():
         toksen = tokenizer.texts_to_sequences(toksen)
         toksen = pad_sequences(toksen, 9)
         predict_array = model.predict(toksen).argmax(axis=1)
-        #{'경상대공지': 0, '공과대공지': 1, '과기대공지': 2, '국문대공지': 3, '디대공지': 4, '디자인대공지': 5, '소프트공지': 6, '약학대공지': 7, '언정대공지': 8}
-        dic = {0:'경상대공지', 1:'공과대공지', 2:'과기대공지', 3:'국문대공지', 4:'디대공지', 5:'디자인대공지', 6:'소프트공지', 7:'약학대공지', 8:'언정대공지'}
+        #{'경상대공지': 0, '공과대공지': 1, '과기대공지': 2, '국문대공지': 3, '디자인대공지': 4, '소프트공지': 5, '약학대공지': 6, '언정대공지': 7}
+        dic = {0:'경상대공지', 1:'공과대공지', 2:'과기대공지', 3:'국문대공지', 4:'디자인대공지', 5:'소프트공지', 6:'약학대공지', 7:'언정대공지'}
         result = [reply, dic[predict_array[0]], dic[predict_array[1]], dic[predict_array[2]] ]
-        msg2 = (result[0]+result[1]+result[2])
+        msg2 = (result[1]+result[2]+result[3])
         log = ChatLog(client=msg, bot=msg2, date=today)
         db.session.add(log)
         db.session.commit()
@@ -63,24 +66,24 @@ def response():
         res = make_response(result)
 
         return res
+    else:
+        try:   #리스트 형태인 경우  > 리스트로 변환
+            reply = ast.literal_eval(reply)
+            log = ChatLog(client=msg, bot=reply[0], date=today) #리스트인 경우 맨 처음에 답 종류 
+            db.session.add(log)
+            db.session.commit()
+            reply = reply[1:]
+            params = {"response": reply}
+        except: 
+            log = ChatLog(client=msg, bot=reply, date=today)
+            db.session.add(log)
+            db.session.commit()
+            params = {"response": reply}
 
-    try:   #리스트 형태인 경우  > 리스트로 변환
-        reply = ast.literal_eval(reply)
-        log = ChatLog(client=msg, bot=reply[0], date=today) #리스트인 경우 맨 처음에 답 종류 
-        db.session.add(log)
-        db.session.commit()
-        reply = reply[1:]
-        params = {"response": reply}
-    except: 
-        log = ChatLog(client=msg, bot=reply, date=today)
-        db.session.add(log)
-        db.session.commit()
-        params = {"response": reply}
+        result = json.dumps(params, ensure_ascii=False)
+        res = make_response(result)
 
-    result = json.dumps(params, ensure_ascii=False)
-    res = make_response(result)
-
-    return res
+        return res
 
     #User.query.filter(User.name.like('%플라스크%')).all() User 모델 테이블에서 name 값 필터링.
     
